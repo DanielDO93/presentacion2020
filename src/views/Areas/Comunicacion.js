fd3749bb1ff4ce923cfb23109b41868a6a355c3f
DanielDO93 <danieldionisio1993@gmail.com>
2020-12-28 13:37:53 -0600

    Update Comunicacion.js

--- src/views/Areas/Comunicacion.js
+++ src/views/Areas/Comunicacion.js
@@ -3,35 +3,164 @@ import "../../index.css";
 import Progress from "../Components/Progress";
 import home from "../../assets/img/home.png";
 import { Row, Col, Container } from 'reactstrap';
+import Nav from 'react-bootstrap/Nav'
+import Tab from 'react-bootstrap/Tab'
+
 class Comercial extends Component {
   render() {
     return (
       <div className="section">
         <div className="slide">
           <div className="content">
-            <h1 className="title2">Comunicación</h1>
+            <Container>
+              <Row>
+                <Col xs="12">
+                  <h2><b className=" top">Cuantitativos: </b></h2>
+                </Col>
+              </Row>
+              <Row>
+                <Tab.Container id="left-tabs-example" defaultActiveKey="uno">
+                  <Row>
+                    <Col sm={12}>
+                      <Nav variant="pills" className="" style={{ backgroundColor: "rgba(0,0,0,0.3)" }}>
+                        <Nav.Item>
+                          <Nav.Link eventKey="uno" className="text-white">De proceso  </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="dos" className="text-white">De proceso  </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="tres" className="text-white">Estratégico </Nav.Link>
+                        </Nav.Item>
+                      </Nav>
+                    </Col>
+                    <Col sm={12}>
+                      <Tab.Content style={{ backgroundColor: "rgba(0,0,0,0.3)" }}>
+                        <Tab.Pane eventKey="uno">
+                          <Row>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Objetivo</b></h2>
+                              <h1>Comunicación Organizacional </h1>
+                            </Col>
+                            <Col xs="12" sm="6" md="4">
+                              <h2> <b>Objetivo</b></h2>
+
+                              <Progress value={95} color="green" />
+                            </Col>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Comentarios</b></h2>
+                              <h1>Auditoría evalúa el indicador </h1>
+                            </Col>
+
+                          </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="dos">
+                          <Row>
+                            <Col xs="12" sm="6" md="6" >
+                              <h2><b>Objetivo</b></h2>
+                              <h1>Aniversarios de campañas </h1>
+                            </Col>
+                            <Col xs="12" sm="6" md="6">
+                              <h2> <b>Objetivo</b></h2>
+
+                              <Progress value={100} color="green" />
+                            </Col>
+
+                          </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="tres">
+                          <Row>
+                            <Col xs="12" sm="6" md="6" >
+                              <h2><b>Objetivo</b></h2>
+                              <h1>Presentaciones corporativas y de clientes &#60; 48 hrs. </h1>
+                            </Col>
+                            <Col xs="12" sm="6" md="6">
+                              <h2> <b>Objetivo</b></h2>
+
+                              <Progress value={95} color="green" />
+                            </Col>
+
+                          </Row>
+                        </Tab.Pane>
+                      </Tab.Content>
+                    </Col>
+                  </Row>
+                </Tab.Container>
+              </Row>
+            </Container>
           </div>
         </div>
-
         <div className="slide">
           <div className="content">
             <Container>
               <Row>
                 <Col xs="12">
-                  <h2><b className="border border-right-0   border-darck">Cumplimiento Plan de trabajo</b></h2>
+                  <h2><b className=" top ">Cualitativos: </b></h2>
                 </Col>
-                <Col xs="12" sm="6" md="4" >
-                  <h2>Objetivo  </h2>
-                  <Progress value={5} color="green" />
-                </Col>
-                <Col xs="12" sm="6" md="4">
-                  <h2> Real</h2>
-                  <Progress value={0} text="No aplica." color="green" />
-                </Col>
-                <Col xs="12" sm="6" md="4">
-                  <h2>% Alcance</h2>
-                  <Progress value={0} text="No aplica." color="green" />
-                </Col>
+              </Row>
+              <Row>
+                <Tab.Container id="left-tabs-example" defaultActiveKey="cuatro">
+                  <Row>
+                    <Col sm={12}>
+                      <Nav variant="pills" className="" style={{ backgroundColor: "rgba(0,0,0,0.3)" }}>
+                        <Nav.Item>
+                          <Nav.Link eventKey="cuatro" className="text-white"> De resultado</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="cinco" className="text-white">De resultado</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="seis" className="text-white">Estratégico</Nav.Link>
+                        </Nav.Item>
+                      </Nav>
+                    </Col>
+                    <Col sm={12}>
+                      <Tab.Content style={{ backgroundColor: "rgba(0,0,0,0.3)" }}>
+                        <Tab.Pane eventKey="cuatro">
+                          <Row>
+                            <Col xs="12" sm="6" md="6" >
+                              <h2><b>Objetivo</b></h2>
+                              <h1>Cumplimiento Plan de trabajo organizacional </h1>
+                            </Col>
+                            <Col xs="12" sm="6" md="6">
+                              <h2> <b>Objetivo</b></h2>
+
+                              <Progress value={0} text="binario" color="green" />
+                            </Col>
+                          </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="cinco">
+                          <Row>
+                            <Col xs="12" sm="6" md="6" >
+                              <h2><b>Objetivo</b></h2>
+                              <h1>Cumplimiento Plan de trabajo corporativo </h1>
+                            </Col>
+                            <Col xs="12" sm="6" md="6">
+                              <h2> <b>Objetivo</b></h2>
+
+                              <Progress value={0} text="binario" color="green" />
+                            </Col>
+                          </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="seis">
+                          <Row>
+                            <Col xs="12" sm="6" md="6" >
+                              <h2><b>Objetivo</b></h2>
+                              <h1>Propuestas de mejora organizacional </h1>
+                            </Col>
+                            <Col xs="12" sm="6" md="6">
+                              <h2> <b>Objetivo</b></h2>
+
+                              <Progress value={0} text="binario" color="green" />
+                            </Col>
+                          </Row>
+                        </Tab.Pane>
+                      
+
+                      </Tab.Content>
+                    </Col>
+                  </Row>
+                </Tab.Container>
               </Row>
             </Container>
           </div>
@@ -39,37 +168,13 @@ class Comercial extends Component {
 
         <div className="slide">
           <div className="content">
-            <Container>
-              <Row>
-                <Col xs="12">
-                  <h2><b className="border border-right-0   border-darck">Rotación</b></h2>
-                </Col>
-                <Col xs="12" sm="6" md="4" >
-                  <h2>Objetivo  </h2>
-                  <Progress value={5} color="green" />
-                </Col>
-                <Col xs="12" sm="6" md="4">
-                  <h2> Real</h2>
-                  <Progress value={0} text="No aplica." color="green" />
-                </Col>
-                <Col xs="12" sm="6" md="4">
-                  <h2>% Alcance</h2>
-                  <Progress value={0} text="No aplica." color="green" />
-                </Col>
-              </Row>
-            </Container>
-          </div>
-        </div>
-
-        <div className="slide">
-          <div className="content">
-          <img
+            <img
               onClick={() => (window.location.href = "/#Indice")}
               src={home}
               alt=""
               style={{
                 width: "15%",
-                cursor:"pointer",
+                cursor: "pointer",
                 filter:
                   "invert(100%) sepia(0%) saturate(7500%) hue-rotate(12deg) brightness(104%) contrast(103%) opacity(65%)"
               }}

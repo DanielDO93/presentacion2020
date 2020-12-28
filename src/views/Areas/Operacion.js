587ffeeeca2a9aab7da153bd7ce44f2a9d1c5c7f
DanielDO93 <danieldionisio1993@gmail.com>
2020-12-28 14:53:01 -0600

    Update Operacion.js

--- src/views/Areas/Operacion.js
+++ src/views/Areas/Operacion.js
@@ -252,6 +252,7 @@ class Operacion extends Component {
             <Container>
               <Row>
                 <Col xs="12">
+                <h2><b>Objetivos anuales 2021</b></h2>
                   <h2><b className=" top">Cuantitativos: </b></h2>
                 </Col>
               </Row>
@@ -437,6 +438,7 @@ class Operacion extends Component {
             <Container>
               <Row>
                 <Col xs="12">
+                <h2><b>Objetivos anuales 2021</b></h2>
                   <h2><b className=" top ">Cualitativos: </b></h2>
                 </Col>
               </Row>

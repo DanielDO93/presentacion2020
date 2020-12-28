9fb645655d7a178c46a06f57e0af9bd8bc174437
DanielDO93 <danieldionisio1993@gmail.com>
2020-12-28 14:53:10 -0600

    Update Comercial.js

--- src/views/Areas/Comercial.js
+++ src/views/Areas/Comercial.js
@@ -146,6 +146,7 @@ class Comercial extends Component {
             <Container>
               <Row>
                 <Col xs="12">
+                <h2><b>Objetivos anuales 2021</b></h2>
                   <h2><b className=" top">Cuantitativos: </b></h2>
                 </Col>
               </Row>
@@ -303,6 +304,7 @@ class Comercial extends Component {
             <Container>
               <Row>
                 <Col xs="12">
+                <h2><b>Objetivos anuales 2021</b></h2>
                   <h2><b className=" top ">Cualitativos: </b></h2>
                 </Col>
               </Row>

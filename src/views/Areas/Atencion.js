c90e7469c78e9e5a7eb2bbddae10ea494f7722aa
DanielDO93 <danieldionisio1993@gmail.com>
2020-12-28 13:57:13 -0600

    Update Atencion.js

--- src/views/Areas/Atencion.js
+++ src/views/Areas/Atencion.js
@@ -23,6 +23,8 @@ class Atencion extends Component {
             <Container>
               <Row>
                 <Col xs="12">
+                <h2><b>Objetivos anuales 2020</b></h2>
+                  <h2><b>1er. Semestre vs. 2do. Semestre</b></h2>
                   <h2><b className=" top">KPI´s: </b></h2>
                 </Col>
               </Row>

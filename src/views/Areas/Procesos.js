001d39b5b0c93be330aeff2cf466901c9a29e463
DanielDO93 <danieldionisio1993@gmail.com>
2020-12-28 13:36:55 -0600

    Update Procesos.js

--- src/views/Areas/Procesos.js
+++ src/views/Areas/Procesos.js
@@ -3,6 +3,8 @@ import "../../index.css";
 import Progress from "../Components/Progress";
 import home from "../../assets/img/home.png";
 import { Row, Col, Container } from 'reactstrap';
+import Nav from 'react-bootstrap/Nav'
+import Tab from 'react-bootstrap/Tab'
 
 class Comercial extends Component {
   render() {
@@ -10,129 +12,313 @@ class Comercial extends Component {
       <div className="section">
         <div className="slide">
           <div className="content">
-            <h1 className="title2">Procesos</h1>
+            <Container>
+              <Row>
+                <Col xs="12">
+                  <h2><b className=" top">Cuantitativos: </b></h2>
+                </Col>
+              </Row>
+              <Row>
+                <Tab.Container id="left-tabs-example" defaultActiveKey="uno">
+                  <Row>
+                    <Col sm={12}>
+                      <Nav variant="pills" className="" style={{ backgroundColor: "rgba(0,0,0,0.3)" }}>
+                        <Nav.Item>
+                          <Nav.Link eventKey="uno" className="text-white">De resultado  </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="dos" className="text-white">De resultado  </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="tres" className="text-white"> De proceso </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="cuatro" className="text-white"> Estratégico </Nav.Link>
+                        </Nav.Item>                       
+                      </Nav>
+                    </Col>
+                    <Col sm={12}>
+                      <Tab.Content style={{ backgroundColor: "rgba(0,0,0,0.3)" }}>
+                        <Tab.Pane eventKey="uno">
+                          <Row>
+                            <Col xs="12" sm="6" md="6" >
+                              <h2><b>Objetivo</b></h2>
+                              <h1>SLA mapeo de procesos </h1>
+                            </Col>
+                            <Col xs="12" sm="6" md="6">
+                              <h2> <b>Objetivo</b></h2>
+
+                              <Progress value={24} text="&#60; 24 hrs"  color="red" />
+                            </Col>
+                           
+                          </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="dos">
+                          <Row>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Objetivo</b></h2>
+                              <h1>SLA de actualización vs. control de cambios </h1>
+                            </Col>
+                            <Col xs="12" sm="6" md="4">
+                              <h2> <b>Objetivo</b></h2>
+                              <Progress value={24} text="&#60; 24 hrs"  color="red" />
+                            </Col>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Comentarios</b></h2>
+                              <h1>vs. tickets levantados </h1>
+                            </Col>
+                           
+                          </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="tres">
+                          <Row>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Objetivo</b></h2>
+                              <h1>Actualización de inventario de procesos</h1>
+                            </Col>
+                            <Col xs="12" sm="6" md="4">
+                              <h2> <b>Objetivo</b></h2>
+                              <Progress value={100} color="green" />
+                            </Col>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Comentarios</b></h2>
+                              <h1>1 revisión por semestre </h1>
+                            </Col>
+                          </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="cuatro">
+                          <Row>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Objetivo</b></h2>
+                              <h1>Propuestas de valor a las campañas</h1>
+                            </Col>
+                            <Col xs="12" sm="6" md="4">
+                              <h2> <b>Objetivo</b></h2>
+                              <Progress value={80} color="green" />
+                            </Col>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Comentarios</b></h2>
+                              <h1>Expecativa de mejora operativa basada en CX </h1>
+                            </Col>
+                          </Row>
+                        </Tab.Pane>
+                                               
+                      </Tab.Content>
+                    </Col>
+                  </Row>
+                </Tab.Container>
+              </Row>
+            </Container>
           </div>
         </div>
-
         <div className="slide">
           <div className="content">
             <Container>
               <Row>
                 <Col xs="12">
-                  <h2><b className="border border-right-0   border-darck">Implementación de NOM35</b></h2>
+                  <h2><b className=" top ">Cualitativos: </b></h2>
                 </Col>
-                <Col xs="12" sm="6" md="4" >
-                  <h2>Objetivo  </h2>
-                  <Progress value={100}   color="green" />
-                </Col>
-                <Col xs="12" sm="6" md="4">
-                  <h2> Real</h2>
-                  <Progress value={10}  color="red" />
-                </Col>
-                <Col xs="12" sm="6" md="4">
-                  <h2>% Alcance</h2>
-                  <Progress value={-90} color="green" />
-                </Col>
+              </Row>
+              <Row>
+                <Tab.Container id="left-tabs-example" defaultActiveKey="cinco">
+                  <Row>
+                    <Col sm={12}>
+                      <Nav variant="pills" className="" style={{ backgroundColor: "rgba(0,0,0,0.3)" }}>
+                        <Nav.Item>
+                          <Nav.Link eventKey="cinco" className="text-white"> De proceso</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="seis" className="text-white"> Estratégico</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="siete" className="text-white"> De resultado</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="ocho" className="text-white"> De resultado</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="nueve" className="text-white"> De proceso</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="diez" className="text-white"> De resultado</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="once" className="text-white"> De proceso</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="doce" className="text-white"> De resultado</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                          <Nav.Link eventKey="trece" className="text-white"> Estratégico</Nav.Link>
+                        </Nav.Item>
+                      </Nav>
+                    </Col>
+                    <Col sm={12}>
+                      <Tab.Content style={{ backgroundColor: "rgba(0,0,0,0.3)" }}>
+                        <Tab.Pane eventKey="cinco">
+                          <Row>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Objetivo</b></h2>
+                              <h1>Implementación de NOM35 </h1>
+                            </Col>
+                            <Col xs="12" sm="6" md="4">
+                              <h2> <b>Objetivo</b></h2>
+
+                              <Progress value={0} text="Binario" color="green" />
+                            </Col>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Comentarios</b></h2>
+                              <h1>Agosto </h1>
+                            </Col>
+                          </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="seis">
+                          <Row>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Objetivo</b></h2>
+                              <h1>Implementación de PCI </h1>
+                            </Col>
+                            <Col xs="12" sm="6" md="4">
+                              <h2> <b>Objetivo</b></h2>
+
+                              <Progress value={0} text="Binario" color="green" />
+                            </Col>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Comentarios</b></h2>
+                              <h1>Octubre </h1>
+                            </Col>
+                          </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="siete">
+                          <Row>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Objetivo</b></h2>
+                              <h1>Auditoría de revisión ISO9001 </h1>
+                            </Col>
+                            <Col xs="12" sm="6" md="4">
+                              <h2> <b>Objetivo</b></h2>
+
+                              <Progress value={0} text="Binario" color="green" />
+                            </Col>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Comentarios</b></h2>
+                              <h1>Renovación en mayo </h1>
+                            </Col>
+                          </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="ocho">
+                          <Row>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Objetivo</b></h2>
+                              <h1>Auditoría de revisión ISO27001 </h1>
+                            </Col>
+                            <Col xs="12" sm="6" md="4">
+                              <h2> <b>Objetivo</b></h2>
+
+                              <Progress value={0} text="Binario" color="green" />
+                            </Col>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Comentarios</b></h2>
+                              <h1>Renovación en febrero </h1>
+                            </Col>
+                          </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="nueve">
+                          <Row>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Objetivo</b></h2>
+                              <h1>Auditoría de Calidad en la recluta </h1>
+                            </Col>
+                            <Col xs="12" sm="6" md="4">
+                              <h2> <b>Objetivo</b></h2>
+
+                              <Progress value={0} text="Binario" color="green" />
+                            </Col>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Comentarios</b></h2>
+                              <h1>Mensual </h1>
+                            </Col>
+                          </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="diez">
+                          <Row>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Objetivo</b></h2>
+                              <h1>Auditar proceso de comunicación organizacional </h1>
+                            </Col>
+                            <Col xs="12" sm="6" md="4">
+                              <h2> <b>Objetivo</b></h2>
+
+                              <Progress value={0} text="Binario" color="green" />
+                            </Col>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Comentarios</b></h2>
+                              <h1>Mensual </h1>
+                            </Col>
+                          </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="once">
+                          <Row>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Objetivo</b></h2>
+                              <h1>Programa de difusión de auditoría por área </h1>
+                            </Col>
+                            <Col xs="12" sm="6" md="4">
+                              <h2> <b>Objetivo</b></h2>
+
+                              <Progress value={0} text="Binario" color="green" />
+                            </Col>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Comentarios</b></h2>
+                              <h1>Enero </h1>
+                            </Col>
+                          </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="doce">
+                          <Row>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Objetivo</b></h2>
+                              <h1>Auditorías Internas de control de cambios </h1>
+                            </Col>
+                            <Col xs="12" sm="6" md="4">
+                              <h2> <b>Objetivo</b></h2>
+
+                              <Progress value={0} text="Binario" color="green" />
+                            </Col>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Comentarios</b></h2>
+                              <h1>Aleatorio mensual </h1>
+                            </Col>
+                          </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="trece">
+                          <Row>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Objetivo</b></h2>
+                              <h1>Auditorías Internas para ISO </h1>
+                            </Col>
+                            <Col xs="12" sm="6" md="4">
+                              <h2> <b>Objetivo</b></h2>
+
+                              <Progress value={0} text="Binario" color="green" />
+                            </Col>
+                            <Col xs="12" sm="6" md="4" >
+                              <h2><b>Comentarios</b></h2>
+                              <h1>1 por semestre </h1>
+                            </Col>
+                          </Row>
+                        </Tab.Pane>
+                        
+                      </Tab.Content>
+                    </Col>
+                  </Row>
+                </Tab.Container>
               </Row>
             </Container>
           </div>
         </div>
 
-        <div className="slide">
-          <div className="content">
-            <Container>
-              <Row>
-                <Col xs="12">
-                  <h2><b className="border border-right-0   border-darck">Implementación de PCI</b></h2>
-                </Col>
-                <Col xs="12" sm="6" md="4" >
-                  <h2>Objetivo  </h2>
-                  <Progress value={100}   color="green" />
-                </Col>
-                <Col xs="12" sm="6" md="4">
-                  <h2> Real</h2>
-                  <Progress value={0}  color="red" />
-                </Col>
-                <Col xs="12" sm="6" md="4">
-                  <h2>% Alcance</h2>
-                  <Progress value={0} color="green" />
-                </Col>
-              </Row>
-            </Container>
-          </div>
-        </div>
-
-        <div className="slide">
-          <div className="content">
-            <Container>
-              <Row>
-                <Col xs="12">
-                  <h2><b className="border border-right-0   border-darck">Auditoría de revisión ISO9001</b></h2>
-                </Col>
-                <Col xs="12" sm="6" md="4" >
-                  <h2>Objetivo  </h2>
-                  <Progress value={0}   color="green" />
-                </Col>
-                <Col xs="12" sm="6" md="4">
-                  <h2> Real</h2>
-                  <Progress value={0}  color="red" />
-                </Col>
-                <Col xs="12" sm="6" md="4">
-                  <h2>% Alcance</h2>
-                  <Progress value={0} color="green" />
-                </Col>
-              </Row>
-            </Container>
-          </div>
-        </div>
-
-        <div className="slide">
-          <div className="content">
-            <Container>
-              <Row>
-                <Col xs="12">
-                  <h2><b className="border border-right-0   border-darck">Auditoría de revisión ISO27001</b></h2>
-                </Col>
-                <Col xs="12" sm="6" md="4" >
-                  <h2>Objetivo  </h2>
-                  <Progress value={100}   color="green" />
-                </Col>
-                <Col xs="12" sm="6" md="4">
-                  <h2> Real</h2>
-                  <Progress value={0}  color="red" />
-                </Col>
-                <Col xs="12" sm="6" md="4">
-                  <h2>% Alcance</h2>
-                  <Progress value={0} color="green" />
-                </Col>
-              </Row>
-            </Container>
-          </div>
-        </div>
-
-        <div className="slide">
-          <div className="content">
-            <Container>
-              <Row>
-                <Col xs="12">
-                  <h2><b className="border border-right-0   border-darck">Auditorías Internas</b></h2>
-                </Col>
-                <Col xs="12" sm="6" md="4" >
-                  <h2>Objetivo  </h2>
-                  <Progress value={2} text="2"  color="green" />
-                </Col>
-                <Col xs="12" sm="6" md="4">
-                  <h2> Real</h2>
-                  <Progress value={0} text="0" color="red" />
-                </Col>
-                <Col xs="12" sm="6" md="4">
-                  <h2>% Alcance</h2>
-                  <Progress value={-2} text="-2" color="green" />
-                </Col>
-              </Row>
-            </Container>
-          </div>
-        </div>
+       
 
         <div className="slide">
           <div className="content">

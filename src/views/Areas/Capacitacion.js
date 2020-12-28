704e258e7269e76db130b00f94a23b2bd77f208d
DanielDO93 <danieldionisio1993@gmail.com>
2020-12-28 14:53:12 -0600

    Update Capacitacion.js

--- src/views/Areas/Capacitacion.js
+++ src/views/Areas/Capacitacion.js
@@ -298,6 +298,7 @@ class Comercial extends Component {
             <Container>
               <Row>
                 <Col xs="12">
+                <h2><b>Objetivos anuales 2021</b></h2>
                   <h2><b className=" top">Cuantitativos: </b></h2>
                 </Col>
               </Row>
@@ -464,6 +465,7 @@ class Comercial extends Component {
             <Container>
               <Row>
                 <Col xs="12">
+                <h2><b>Objetivos anuales 2021</b></h2>
                   <h2><b className=" top ">Cualitativos: </b></h2>
                 </Col>
               </Row>

82268118a9e15123f79b6e7874cb70ca7979a458
DanielDO93 <danieldionisio1993@gmail.com>
2020-12-28 14:53:07 -0600

    Update CommandCenter.js

--- src/views/Areas/CommandCenter.js
+++ src/views/Areas/CommandCenter.js
@@ -114,6 +114,7 @@ class Comercial extends Component {
             <Container>
               <Row>
                 <Col xs="12">
+                <h2><b>Objetivos anuales 2021</b></h2>
                   <h2><b className=" top">Cuantitativos: </b></h2>
                 </Col>
               </Row>
@@ -201,6 +202,7 @@ class Comercial extends Component {
             <Container>
               <Row>
                 <Col xs="12">
+                <h2><b>Objetivos anuales 2021</b></h2>
                   <h2><b className=" top ">Cualitativos: </b></h2>
                 </Col>
               </Row>
